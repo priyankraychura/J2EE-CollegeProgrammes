506baa19467c5d3844598e8450c6e1111e489956
priyankraychura <priyankraychura@gmail.com>
2024-07-13 20:43:16 +0530

    modified

--- src/Test_Practice/Main.java
+++ src/Test_Practice/Main.java
@@ -5,10 +5,15 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        Student s1 = new Student("Priyank", "pr@12345");
+        Student s1 = new Student("Pruthvi", "pruthvi@12345");
+        Student s2 = new Student("Priyank", "pr@12345");
+//        s1.setRollNo(1);
 
         try {
+//            dao.updateStudent(s1);
+//            dao.deleteStudent(1);
             dao.insertStudent(s1);
+            dao.insertStudent(s2);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/Test_Practice/StudentDAO.java
+++ src/Test_Practice/StudentDAO.java
@@ -30,4 +30,62 @@ public class StudentDAO {
             conn.close();
         }
     }
+
+    public void updateStudent(Student st) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            String SQL = "UPDATE student SET name = ?, password = ? WHERE id = ?";
+            stmt = conn.prepareStatement(SQL);
+
+            stmt.setString(1, st.getName());
+            stmt.setString(2, st.getPassword());
+            stmt.setInt(3, st.getRollNo());
+
+            int rowsUpdated = stmt.executeUpdate();
+
+            if(rowsUpdated > 0){
+                System.out.println("Student updated successfully");
+            } else{
+                System.out.println("Student update failed");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            stmt.close();
+            conn.close();
+        }
+
+
+    }
+
+    public void deleteStudent(int id) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            String SQL = "DELETE FROM student WHERE id = ?";
+            stmt = conn.prepareStatement(SQL);
+
+            stmt.setInt(1, id);
+
+            int rowsUpdated = stmt.executeUpdate();
+
+            if(rowsUpdated > 0){
+                System.out.println("Student deleted successfully");
+            } else{
+                System.out.println("Student delete failed");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            stmt.close();
+            conn.close();
+        }
+    }
 }
